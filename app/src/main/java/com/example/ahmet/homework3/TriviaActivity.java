cedea59864a4172f2bd95adac0aeb4adf4d01d7a
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2016-09-21 15:00:56 -0400

    AddRadioButton function implemented

--- app/src/main/java/com/example/ahmet/homework3/TriviaActivity.java
+++ app/src/main/java/com/example/ahmet/homework3/TriviaActivity.java
@@ -1,13 +1,28 @@
 package com.example.ahmet.homework3;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 
 public class TriviaActivity extends AppCompatActivity {
 
+    private RadioGroup choicesRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia);
+
+        choicesRadioGroup = (RadioGroup) findViewById(R.id.radioGroupChoices);
+    }
+
+    private void addRadioButton(String radioButtonText){
+        RadioButton radioButton = new RadioButton(this);
+        radioButton.setText(radioButtonText);
+        choicesRadioGroup.addView(radioButton,new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 }
